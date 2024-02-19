c962006b166d63d9f87e2e6be4230930af3dacec
saviyola-shajan <saviyolashajan69@gmail.com>
2024-02-19 09:47:52 +0530

    326

---
+++ leetcode/easy/326.js
@@ -0,0 +1,11 @@
+// Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+// An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+var isPowerOfThree = function(n) {
+    let x=0;
+    while(3**x<n){
+        x++
+    }
+    return 3**x===n
+};
