5f20711a11dcb2cd7ef771e61dbac21153ba7155
saviyola-shajan <saviyolashajan69@gmail.com>
2024-03-12 18:07:11 +0530

    2621

---
+++ leetcode/easy/2621.js
@@ -0,0 +1,9 @@
+// Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value.
+
+async function sleep(millis) {
+    return new Promise((resolve,reject)=>{
+     setTimeout(()=>{
+         resolve(millis)
+     },millis);
+    }) 
+ }
