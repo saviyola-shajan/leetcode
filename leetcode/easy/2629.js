b6da2c4a39f3f57d9e237e7414ccc6fbc1e81498
saviyola-shajan <saviyolashajan69@gmail.com>
2024-03-13 13:48:10 +0530

    2629

---
+++ leetcode/easy/2629.js
@@ -0,0 +1,18 @@
+// Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+// The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+// The function composition of an empty list of functions is the identity function f(x) = x.
+
+// You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+var compose = function (functions) {
+
+    return function (x) {
+        let result = x
+        for (let i = functions.length - 1; i >= 0; i--) {
+            result = functions[i](result)
+        }
+        return result
+    }
+};
