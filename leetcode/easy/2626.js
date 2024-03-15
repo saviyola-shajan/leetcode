f4dfef8dab61faf2570ca73c158915ad03c3add2
saviyola-shajan <saviyolashajan69@gmail.com>
2024-03-15 11:11:22 +0530

    2626

---
+++ leetcode/easy/2626.js
@@ -0,0 +1,16 @@
+// Given an integer array nums, a reducer function fn, and an initial value init, return the final result obtained by executing the fn function on each element of the array, sequentially, passing in the return value from the calculation on the preceding element.
+
+// This result is achieved through the following operations: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The ultimate value of val is then returned.
+
+// If the length of the array is 0, the function should return init.
+
+// Please solve it without using the built-in Array.reduce method.
+
+
+var reduce = function (nums, fn, init) {
+    let res = init
+    for (let i = 0; i < nums.length; i++) {
+        res = fn(res, nums[i])
+    }
+    return res
+};
