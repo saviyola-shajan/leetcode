29222379c267a70d491be0e0dcbc4ac9af87647d
saviyola-shajan <saviyolashajan69@gmail.com>
2024-03-22 13:09:13 +0530

    2651

---
+++ leetcode/easy/2651.js
@@ -0,0 +1,13 @@
+// You are given a positive integer arrivalTime denoting the arrival time of a train in hours, and another positive integer delayedTime denoting the amount of delay in hours.
+
+// Return the time when the train will arrive at the station.
+
+// Note that the time in this problem is in 24-hours format.
+
+
+var findDelayedArrivalTime = function (arrivalTime, delayedTime) {
+    const arrive = delayedTime + arrivalTime
+    if (arrive < 24) 
+    return arrive
+    return arrive - 24
+};
