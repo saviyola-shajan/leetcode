60901f8b8f0094088394a4a42a9ae373967e8819
saviyola-shajan <saviyolashajan69@gmail.com>
2024-03-23 14:42:03 +0530

    2652

---
+++ leetcode/easy/2652.js
@@ -0,0 +1,14 @@
+// Given a positive integer n, find the sum of all integers in the range [1, n] inclusive that are divisible by 3, 5, or 7.
+
+// Return an integer denoting the sum of all numbers in the given range satisfying the constraint.
+
+
+var sumOfMultiples = function (n) {
+    let sum = 0
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
+            sum += i
+        }
+    }
+    return sum
+};
