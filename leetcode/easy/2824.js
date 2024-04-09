5d4c5228cd40af04b9fc91a9325cd3e7a0c3caea
saviyola-shajan <saviyolashajan69@gmail.com>
2024-04-09 18:45:25 +0530

    2824

---
+++ leetcode/easy/2824.js
@@ -0,0 +1,14 @@
+// Given a 0-indexed integer array nums of length n and an integer target, return the number of pairs (i, j) where 0 <= i < j < n and nums[i] + nums[j] < target.
+
+
+var countPairs = function (nums, target) {
+    let count = 0
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] < target) {
+                count++
+            }
+        }
+    }
+    return count
+};
