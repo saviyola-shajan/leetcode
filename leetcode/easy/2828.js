90c30c82d467f28cfc9788a329da5095a7c1cffc
saviyola-shajan <saviyolashajan69@gmail.com>
2024-04-10 12:27:36 +0530

    2828

---
+++ leetcode/easy/2828.js
@@ -0,0 +1,13 @@
+// Given an array of strings words and a string s, determine if s is an acronym of words.
+
+// The string s is considered an acronym of words if it can be formed by concatenating the first character of each string in words in order. For example, "ab" can be formed from ["apple", "banana"], but it can't be formed from ["bear", "aardvark"].
+
+// Return true if s is an acronym of words, and false otherwise.
+
+var isAcronym = function (words, s) {
+    if (words.length !== s.length) return false
+    for (let i = 0; i < words.length; i++) {
+        if (words[i][0] !== s[i]) return false
+    }
+    return true
+};
