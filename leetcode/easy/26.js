fb90accaf1f1840036bcfb0371e397cfb883eb77
saviyola-shajan <saviyolashajan69@gmail.com>
2024-04-18 12:21:30 +0530

    26

---
+++ leetcode/easy/26.js
@@ -0,0 +1,32 @@
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Custom Judge:
+
+// The judge will test your solution with the following code:
+
+// int[] nums = [...]; // Input array
+// int[] expectedNums = [...]; // The expected answer with correct length
+
+// int k = removeDuplicates(nums); // Calls your implementation
+
+// assert k == expectedNums.length;
+// for (int i = 0; i < k; i++) {
+//     assert nums[i] == expectedNums[i];
+// }
+// If all assertions pass, then your solution will be accepted.
+
+
+var removeDuplicates = function (nums) {
+    let k = 1
+    for (let i = 0; i < nums.length-1; i++) {
+        if (nums[i] !== nums[i + 1]) {
+            nums[k] = nums[i + 1]
+        k++
+        }
+    }
+    return k
+};
